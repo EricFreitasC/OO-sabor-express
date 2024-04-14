bf4fa9e1a262a790e6251bedef4c055100a58ede
ericc <ericdennisfc@gmail.com>
2024-04-13 21:53:59 -0300

    Projeto poo saor express

---
+++ modelos/restaurante.py
@@ -0,0 +1,11 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+
+restaurante_praça = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praça, restaurante_pizza]
+print(restaurantes)
