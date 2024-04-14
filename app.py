13ccae9045a88208283ea4bf5fdf79d1fb62599a
Eric Freitas <ericdennisfc@gmail.com>
2024-04-14 11:53:01 -0300

    classe avaliação adicionada e ajustes na classe restaurante adicionadno avaliação

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praça = Restaurante('Praça', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
-
-restaurante_mexicano.alternar_estado()
+restaurante_praça.receber_avaliacao('gui', 10)
+restaurante_praça.receber_avaliacao('larissa', 8)
+restaurante_praça.receber_avaliacao('emy', 5)
 
 def main():
     Restaurante.listar_restaurantes()
