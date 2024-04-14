641a94a98d525dc352004ec906c4d10a662c760e
Eric Freitas <ericdennisfc@gmail.com>
2024-04-14 09:52:53 -0300

    construtor e metodos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,20 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
+    def __init__(self, nome, cartegoria):
+        self.nome = nome
+        self.categoria = cartegoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praça = Restaurante()
-restaurante_praça.nome = 'Praça'
-restaurante_praça.categoria = 'Gourmet'
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes ():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-restaurante_pizza = Restaurante()
+restaurante_praça = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praça, restaurante_pizza]
-print(vars(restaurante_praça))
+Restaurante.listar_restaurantes()
